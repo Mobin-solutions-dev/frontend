d051e47bf997f7d4c55ab9bedf34715cf7764f59
Matthieu Lallai <matthieu.lallai@gmail.com>
2021-01-22 11:44:36 +0100

    changed webpack config + sdb lib

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
     target: "serverless",
     images: {
         domains: [process.env.CLOUDINARY_URL],
+    },
+    webpack: (config, options) => {
+        config.module.rules.push({ parser: { amd: false } })
+        return config
     }
 }
