6727d7b3f19defbb025f31185a371c3eb6c8a614
Matthieu Lallai <matthieu.lallai@gmail.com>
2021-09-24 16:12:04 +0200

    added mobilitances page

--- utils/menu/menu-items/index.js
+++ utils/menu/menu-items/index.js
@@ -1,105 +1,109 @@
 export const menuItems = [
-    {
-        id: 1,
-        // icon: "Mob'in",
-        title: "Mob'In",
-        subItems: [
-            {
-                title: "Présentation",
-                link: "/presentation"
-            },
-            {
-                title: "Notre histoire, nos valeurs et nos missions",
-                link: "/histoire-valeurs-missions"
-            },
-            {
-                title: "Notre fonctionnement",
-                link: "/fonctionnement"
-            },
-            {
-                title: "Nos adhérents",
-                link: "/adherents"
-            },
-            {
-                title: "Nos actualités",
-                link: "/articles"
-            },
-            {
-                title: "Nos partenaires",
-                link: "/partenaires"
-            },
-            {
-                title: "Contact",
-                link: "/contact"
-            },
-        ]
-    },
-    {
-        id: 2,
-        title: "Nos activités",
-        subItems: [
-            {
-                title: "savoir bouger",
-                link: "/savoir-bouger"
-            },
-            {
-                title: "pouvoir bouger",
-                link: "/pouvoir-bouger"
-            },
-        ]
-    },
-    {
-        id: 3,
-        title: "Nos projets",
-        subItems: [
-            {
-                title: "Intégracode",
-                link: "/projets/integracode"
-            },
-            {
-                title: "Integramob",
-                link: "/projets/integramob"
-            },
-            {
-                title: "Nos formations",
-                link: "/formations"
-            },
-        ]
-    },
-    {
-        id: 4,
-        title: "Notre accompagnement",
-        subItems: [
-            {
-                title: "Au service des territoires",
-                link: "/accompagnement/territoires"
-            },
-            {
-                title: "Au service des entreprises",
-                link: "/accompagnement/entreprises"
-            },
-            // {
-            //     title: "Devenir partenaire",
-            //     link: "/devenir-partenaire"
-            // },
-        ]
-    },
-]
+  {
+    id: 1,
+    // icon: "Mob'in",
+    title: "Mob'In",
+    subItems: [
+      {
+        title: "Présentation",
+        link: "/presentation",
+      },
+      {
+        title: "Notre histoire, nos valeurs et nos missions",
+        link: "/histoire-valeurs-missions",
+      },
+      {
+        title: "Notre fonctionnement",
+        link: "/fonctionnement",
+      },
+      {
+        title: "Nos adhérents",
+        link: "/adherents",
+      },
+      {
+        title: "Nos actualités",
+        link: "/articles",
+      },
+      {
+        title: "Nos partenaires",
+        link: "/partenaires",
+      },
+      {
+        title: "Contact",
+        link: "/contact",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Nos activités",
+    subItems: [
+      {
+        title: "savoir bouger",
+        link: "/savoir-bouger",
+      },
+      {
+        title: "pouvoir bouger",
+        link: "/pouvoir-bouger",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Nos projets",
+    subItems: [
+      {
+        title: "Intégracode",
+        link: "/projets/integracode",
+      },
+      {
+        title: "Integramob",
+        link: "/projets/integramob",
+      },
+      {
+        title: "M[ob]ilitances",
+        link: "/projets/m[ob]ilitances",
+      },
+      {
+        title: "Nos formations",
+        link: "/formations",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Notre accompagnement",
+    subItems: [
+      {
+        title: "Au service des territoires",
+        link: "/accompagnement/territoires",
+      },
+      {
+        title: "Au service des entreprises",
+        link: "/accompagnement/entreprises",
+      },
+      // {
+      //     title: "Devenir partenaire",
+      //     link: "/devenir-partenaire"
+      // },
+    ],
+  },
+];
 
 export const menuLoginItems = [
-    {
-        id: 1000,
-        title: "Mon compte",
-        subItems: [
-            {
-                title: "Accueil Adhérent",
-                link: "/auth"
-            },
-            {
-                title: "Déconnexion",
-                link: "/",
-                action: "Logout"
-            },
-        ]
-    },
-]
+  {
+    id: 1000,
+    title: "Mon compte",
+    subItems: [
+      {
+        title: "Accueil Adhérent",
+        link: "/auth",
+      },
+      {
+        title: "Déconnexion",
+        link: "/",
+        action: "Logout",
+      },
+    ],
+  },
+];
--- utils/text/index.js
+++ utils/text/index.js
@@ -1,3 +1,4 @@
-export * from './accompagner-territoires'
-export * from './integracode'
-export * from "./integramob"
+export * from "./accompagner-territoires";
+export * from "./integracode";
+export * from "./integramob";
+export * from "./mobilitances";
